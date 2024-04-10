da05b032f6fe5d70ae35bef2926f4a9416eb4783
coco-ball <kidbean02@snu.ac.kr>
2024-04-10 19:28:45 +0900

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header></Header>
+      <Outlet></Outlet>;
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,3 @@
+export default function Header() {
+  return <div className="bg-slate-100 h-10">Header</div>;
+}
