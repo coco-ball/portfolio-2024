61906bf8224464fe68aa4a4558619db3ca73270f
coco-ball <kidbean02@snu.ac.kr>
2025-01-21 06:06:32 +0900

    Fix: Build

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/chaewonyu",
+  base: "/portfolio-2024",
   plugins: [react(), tsconfigPaths()],
   css: {
     postcss: {
